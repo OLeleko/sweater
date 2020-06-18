8c6a6814e27628c3a103ba0a222eb684e02928ee
Oleksandr Leleko <o.leleko@smiddle.lab>
2020-06-18 17:40:55 +0300

    Third commit

--- src/main/java/com/example/sweater/GreetingController.java
+++ src/main/java/com/example/sweater/GreetingController.java
@@ -1,14 +1,21 @@
 package com.example.sweater;
 
+import com.example.sweater.domain.Message;
+import com.example.sweater.repos.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
 public class GreetingController {
+    @Autowired
+    private MessageRepo messageRepo;
+
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required = false, defaultValue = "World")
                                        String name, Map<String, Object> model){
@@ -18,8 +25,36 @@ public class GreetingController {
 
     @GetMapping
     public String Man(Map<String, Object> model){
-        model.put("some", "Hello, letsCode");
+        Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages", messages);
         return "man";
+    }
+
+    @PostMapping
+    public String add(@RequestParam String text, @RequestParam String tag,  Map<String, Object> model){
+        Message message = new Message(text, tag);
+        messageRepo.save(message);
+        Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages", messages);
+
+        return "man";
+
+    }
+
+    @PostMapping("/filter")
+    public String filter(@RequestParam String filter, Map<String, Object> model){
+        Iterable<Message> messages ;
+        if(!filter.isEmpty() && filter != null ){
+            messages = messageRepo.findByTag(filter);
+        }else {
+            messages = messageRepo.findAll();
+        }
+        model.put("messages", messages);
+
+
+
+        return "man";
+
     }
 
 }
