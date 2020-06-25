a34a23f405f1ca809fb71d8592d4ec2697ab7324
Oleksandr Leleko <o.leleko@smiddle.lab>
2020-06-25 17:16:22 +0300

    Admin role add commit

--- src/main/java/com/example/sweater/repos/UserRepo.java
+++ src/main/java/com/example/sweater/repos/UserRepo.java
@@ -3,6 +3,7 @@ package com.example.sweater.repos;
 import com.example.sweater.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepo extends JpaRepository<User, Long> {
+public interface UserRepo extends JpaRepository<User, Integer> {
     User findByUsername(String username);
+    User findById(int id);
 }
